5fbea7fb3374cd7860567feffeb0423af1ab1d8b
Wut Yee Tun <wutyee.tun@innovate.com>
2021-02-05 14:55:55 +0630

    add the township and city model

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function township()
+    {
+        return $this->hasMany(Township::class);
+    }
 }
