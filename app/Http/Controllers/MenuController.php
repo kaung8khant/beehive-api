1248377b174138fb44a83f8ed07e549495e42a50
Nang Htet Oo Wai <nanghtetoo.wai@hiveinnovate.com>
2021-02-09 13:01:21 +0630

    menu add

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request['slug'] = $this->generateUniqueSlug();
+
+        $menu = Menu::create($request->validate([
+            'name' => 'required|unique:menus',
+            'name_mm' => 'required|unique:menus',
+            'slug' => 'required|unique:menus',
+            // 'restaurantCategoy_id' => 'required|exists:App\Models\RestaurantCategory,id',
+        ]));
+
+        return response()->json($menu, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Menu $menu)
+    {
+        return Menu::with('restaurantCategory')->where('slug', $slug)->firstOrFail();
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Menu $menu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Menu $menu)
+    {
+        $menu = Menu::where('slug', $slug)->firstOrFail();
+
+        $menu->update($request->validate([
+            'name' => [
+                'required',
+                Rule::unique('menus')->ignore($menu->id),
+            ],
+            'name_mm' => [
+                'required',
+                Rule::unique('menus')->ignore($menu->id),
+            ],
+            // 'restaurantCategory_id' => 'required|exists:App\Models\RestaurantCategory,id',
+        ]));
+
+        return response()->json($subCategory, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Menu  $menu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Menu $menu)
+    {
+        Menu::where('slug', $slug)->firstOrFail()->delete();
+        return response()->json(['message' => 'Successfully deleted.'], 200);
+    }
+}
---
+++ database/migrations/2021_02_09_054547_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('slug')->unique();
+            $table->string('name')->unique();
+            $table->string('name_mm')->unique();
+            $table->string('descrption')->unique();
+            $table->string('description_mm')->unique();
+            // $table->unsignedBigInteger('restaurantCategory_id');
+            // $table->foreign('restaurantCategory_id')->references('id')->on('restaurantCategories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
