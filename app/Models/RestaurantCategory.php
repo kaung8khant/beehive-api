815d96253d58f908b495dcfbe7b5ca02e48d73ab
Wut Yee Tun <wutyee.tun@innovate.com>
2021-02-09 17:14:36 +0630

    add the restaurantseeder

--- app/Models/RestaurantCategory.php
+++ app/Models/RestaurantCategory.php
@@ -10,4 +10,21 @@ class RestaurantCategory extends Model
     use HasFactory;
 
     protected $fillable = ['name','name_mm', 'slug'];
+
+    /**
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+        'pivot',
+    ];
+
+    public function restaurants()
+    {
+        return $this->belongsToMany(Restaurant::class, 'category_restaurant');
+    }
 }
