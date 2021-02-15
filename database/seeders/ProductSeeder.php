86bc73230cb96be5fa04e0c0d96278ce02508c6a
Zin Win Naing <zinwin.naing@hiveinnovate.com>
2021-02-15 17:40:51 +0630

    BH-305 BH-306 BH-290 BH-302 finish

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -38,8 +38,8 @@ class ProductSeeder extends Seeder
                 "price" => 60000,
                 "slug" => $this->generateUniqueSlug(),
                 "shop_id" => 1,
-                "shop_category_id"=>2,
-                "sub_category_id"=>2
+                "shop_category_id"=>1,
+                "sub_category_id"=>1
 
             ],
         ];
