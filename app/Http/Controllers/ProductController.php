54e1d15e5793b7e00cc086322d68e2bec15b6b44
Aye Myat Thin aung <ayemyatthin.aung@hiveinnovate.com>
2021-02-26 11:10:13 +0630

    changed product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -136,7 +136,6 @@ class ProductController extends Controller
         return Product::whereHas('shop', function ($q) use ($slug) {
             $q->where('slug', $slug);
         })->where('name', 'LIKE', '%' . $request->filter . '%')
-        ->orWhere('name_mm', 'LIKE', '%' . $request->filter . '%')
         ->orWhere('slug', $request->filter)->paginate(10);
     }
 
