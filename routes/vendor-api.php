bca55438a975cc91a12e5a60616fa673c2734ac0
tuntinzarthin <tuntinzar.thin@hiveinnovate.com>
2021-03-09 11:48:04 +0630

    restaurant branch end point

--- routes/vendor-api.php
+++ routes/vendor-api.php
@@ -9,5 +9,24 @@ Route::group(['prefix' => 'vendor'], function () {
         Route::put('profile/update', 'Auth\UserAuthController@updateProfile');
         Route::post('refresh-token', 'Auth\UserAuthController@refreshToken');
         Route::post('logout', 'Auth\UserAuthController@logout');
+
+        /* restaurant */
+
+        /* branch */
+        Route::get('restaurants/{slug}/restaurant-branches', 'RestaurantBranchController@getBranchesByRestaurant');
+        Route::get('restaurant-branches/{slug}', 'RestaurantBranchController@show');
+        Route::post('restaurant-branches', 'RestaurantBranchController@store');
+        Route::put('restaurant-branches/{slug}', 'RestaurantBranchController@update');
+        Route::patch('restaurant-branches/toggle-enable/{slug}', 'RestaurantBranchController@toggleEnable');
+        Route::delete('restaurant-branches/{slug}', 'RestaurantBranchController@destory');
+        /* branch */
+
+        /* menus */
+        Route::get('restaurant-branches/{slug}/menus', 'MenuController@getAvailableMenusByRestaurantBranch');
+        Route::post('restaurant-branches/add-available-menus/{slug}', 'RestaurantBranchController@addAvailableMenus');
+        Route::post('restaurant-branches/remove-available-menus/{slug}', 'RestaurantBranchController@removeAvailableMenus');
+        /* menus */
+
+        /* restaurant */
     });
 });
