81c25a494471534e84ab93f4e1725070fff0d4bb
tuntinzarthin <tuntinzar.thin@hiveinnovate.com>
2021-03-09 12:05:13 +0630

    vendor restaurant menus end point

--- routes/vendor-api.php
+++ routes/vendor-api.php
@@ -22,9 +22,17 @@ Route::group(['prefix' => 'vendor'], function () {
         /* branch */
 
         /* menus */
+        Route::get('restaurants/{slug}/menus', 'MenuController@getMenusByRestaurant');
         Route::get('restaurant-branches/{slug}/menus', 'MenuController@getAvailableMenusByRestaurantBranch');
+        Route::get('menus/{slug}', 'MenuController@show');
+        Route::post('menus', 'MenuController@store');
+        Route::put('menus/{slug}', 'MenuController@update');
+        Route::delete('menus/{slug}', 'MenuController@destory');
+        Route::patch('menus/toggle-enable/{slug}', 'MenuController@toggleEnable');
         Route::post('restaurant-branches/add-available-menus/{slug}', 'RestaurantBranchController@addAvailableMenus');
         Route::post('restaurant-branches/remove-available-menus/{slug}', 'RestaurantBranchController@removeAvailableMenus');
+        Route::get('menus/{slug}/menu-variations', 'MenuVariationController@getVariationsByMenu');
+        Route::get('menus/{slug}/menu-toppings', 'MenuToppingController@getToppingsByMenu');
         /* menus */
 
         /* restaurant */
