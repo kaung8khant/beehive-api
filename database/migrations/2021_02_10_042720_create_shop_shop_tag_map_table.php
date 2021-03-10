8c31f64c31070ce6db61ce5cb97f9cd8ccadc1aa
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-03-10 17:51:25 +0630

    fix migration file names

--- database/migrations/2021_02_10_042720_create_shop_shop_tag_map_table.php
+++ database/migrations/2021_02_10_042720_create_shop_shop_tag_map_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagShopTable extends Migration
+class CreateShopShopTagMapTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_02_27_075808_create_restaurant_branch_menu_map_table.php
+++ database/migrations/2021_02_27_075808_create_restaurant_branch_menu_map_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRestaurantBranchMenuTable extends Migration
+class CreateRestaurantBranchMenuMapTable extends Migration
 {
     /**
      * Run the migrations.
