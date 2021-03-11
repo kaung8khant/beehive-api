78c3994e1146037469bf2cc35421088af40828e6
Zin Win Naing <zinwin.naing@hiveinnovate.com>
2021-03-11 18:39:56 +0630

    add swagger in menutopping

--- app/Models/MenuTopping.php
+++ app/Models/MenuTopping.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *      @OA\Xml(name="MenuTopping"),
+ *      @OA\Property(property="name", type="string", example="Name"),
+ *      @OA\Property(property="name_mm", type="string", example="အမည်"),
+ *      @OA\Property(property="menu_slug", type="string", example="D16AAF"),
+ *      @OA\Property(property="price", type="number", example=1000),
+ *      @OA\Property(property="slug", type="string", readOnly=true)
+ * )
+ */
+
 class MenuTopping extends Model
 {
     use HasFactory;
