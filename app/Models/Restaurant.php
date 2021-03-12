889207cd19f7e8c32648a60859a6dd04d52cab39
Nang Htet Oo Wai <nanghtetoo.wai@hiveinnovate.com>
2021-03-12 14:53:13 +0630

    restaurant schema fix

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -24,6 +24,8 @@ use Illuminate\Database\Eloquent\Model;
  *      @OA\Property(property="closing_time", type="time",example="00:00"),
  *      @OA\Property(property="township_slug", type="string", example="township_slug"),
  *      @OA\Property(property="address", type="string",example="address"),
+ *      @OA\Property(property="latitude", type="double", example="0.00"),
+ *      @OA\Property(property="longitude", type="double", example="0.00"),
  *      @OA\Property(property="is_enable", type="boolean", example="true"),
  *      @OA\Property(property="slug", type="string", readOnly=true)
  *       ),
