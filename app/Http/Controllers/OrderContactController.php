2ad84c913e43d48a470c01c2ae71a02564b973e6
Zin Win Naing <zinwin.naing@hiveinnovate.com>
2021-03-15 14:18:10 +0630

    fixed in order swagger code and add  swagger in ordercontact

--- app/Http/Controllers/OrderContactController.php
+++ app/Http/Controllers/OrderContactController.php
@@ -11,6 +11,31 @@ class OrderContactController extends Controller
 {
     use StringHelper;
 
+     /**
+     * @OA\Get(
+     *      path="/api/v2/admin/orders/{slug}/contact",
+     *      operationId="Order",
+     *      tags={"OrderContact"},
+     *      summary="Get One Order Contact",
+     *      description="Returns a requested order contact",
+     *      @OA\Parameter(
+     *          name="slug",
+     *          description="Slug of a requested order",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation"
+     *      ),
+     *      security={
+     *          {"bearerAuth": {}}
+     *      }
+     *)
+     */
     public function show($slug)
     {
         return OrderContact::whereHas('order', function ($q) use ($slug) {
@@ -18,6 +43,39 @@ class OrderContactController extends Controller
         })->firstOrFail();
     }
 
+     /**
+     * @OA\Put(
+     *      path="/api/v2/admin/orders/{slug}/contact",
+     *      operationId="updateOrderContact",
+     *      tags={"OrderContact"},
+     *      summary="Update a OrderContact",
+     *      description="Update a requested OrderContact",
+     *      @OA\Parameter(
+     *          name="slug",
+     *          description="Slug to identify a Order",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="New Order Contact data to be updated.",
+     *          @OA\MediaType(
+     *              mediaType="applications/json",
+     *              @OA\Schema(ref="#/components/schemas/OrderContact")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation"
+     *      ),
+     *      security={
+     *          {"bearerAuth": {}}
+     *      }
+     *)
+     */
     public function update(Request $request, $slug)
     {
         $order = Order::where('slug', $slug)->firstOrFail();
