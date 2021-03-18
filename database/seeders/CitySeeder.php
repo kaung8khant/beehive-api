defbb3ce21df5edeb938db9c20360576320bbc47
Nang Htet Oo Wai <nanghtetoo.wai@hiveinnovate.com>
2021-03-18 15:28:14 +0630

    city seeder add

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Helpers\StringHelper;
 use App\Models\City;
-use App\Models\Township;
 
 class CitySeeder extends Seeder
 {
@@ -18,8 +17,22 @@ class CitySeeder extends Seeder
      */
     public function run()
     {
-        City::factory()->count(3)->has(Township::factory()->count(3))->create();
-        City::factory()->count(3)->has(Township::factory()->count(5))->create();
-        City::factory()->count(10)->create();
+        $cities = [
+            [
+                'name' => 'Test',
+                'slug' => $this->generateUniqueSlug(),
+            ],
+            [
+                'name' => 'Test',
+                'slug' => $this->generateUniqueSlug(),
+            ],
+        ];
+
+        foreach ($cities as $city) {
+            City::create($city);
+        }
+        // City::factory()->count(3)->has(Township::factory()->count(3))->create();
+        // City::factory()->count(3)->has(Township::factory()->count(5))->create();
+        // City::factory()->count(10)->create();
     }
 }
