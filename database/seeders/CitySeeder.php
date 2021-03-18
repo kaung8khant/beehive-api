6df4fde85bff1bee5d1d044a149b903c2294a14e
Nang Htet Oo Wai <nanghtetoo.wai@hiveinnovate.com>
2021-03-18 15:47:43 +0630

    git add .

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Helpers\StringHelper;
 use App\Models\City;
+use App\Models\Township;
+
+use function Ramsey\Uuid\v1;
 
 class CitySeeder extends Seeder
 {
@@ -17,22 +20,82 @@ class CitySeeder extends Seeder
      */
     public function run()
     {
-        $cities = [
-            [
-                'name' => 'Test',
-                'slug' => $this->generateUniqueSlug(),
-            ],
-            [
-                'name' => 'Test',
-                'slug' => $this->generateUniqueSlug(),
-            ],
-        ];
+        // $cities = [
+        //     [
+        //         'name' => 'Kachin',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Kaya',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Chin',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Mon',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Yakhine',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Eastern Shan',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Southern Shan',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Nothern Shan',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Sagaing',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Tanintharyi',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Nay Pyi Taw',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Pago(N)',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Pago',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Magway',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Mandalay',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Yangon',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        //     [
+        //         'name' => 'Ayeyarwaddy',
+        //         'slug' => $this->generateUniqueSlug(),
+        //     ],
+        // ];
 
-        foreach ($cities as $city) {
-            City::create($city);
-        }
-        // City::factory()->count(3)->has(Township::factory()->count(3))->create();
-        // City::factory()->count(3)->has(Township::factory()->count(5))->create();
-        // City::factory()->count(10)->create();
+        // foreach ($cities as $city) {
+        //     City::create($city);
+        // }
+        City::factory()->count(3)->has(Township::factory()->count(3))->create();
+        City::factory()->count(3)->has(Township::factory()->count(5))->create();
+        City::factory()->count(10)->create();
     }
 }
