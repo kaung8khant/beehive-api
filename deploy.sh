585b590ab9a0516d0c222343c6d694d0a82f97d8
Aung Thu Moe <aungthu.moe@hiveinnovate.com>
2021-04-03 17:19:08 +0630

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-sudo -u beehive bash <<EOF
-  # Change to working directory
-  cd /var/www/html/beehive-api
 
-  # Pull the latest changes from the git repository
-  git checkout -- .
-  git clean -df
-  git pull
+# Install composer dependencies
+composer install
 
-  # Install composer dependencies
-  composer install
+# Run database migration
+php artisan migrate:fresh --seed
 
-  # Run database migration
-  php artisan migrate:fresh --seed
+# Optimize configuration loading
+php artisan config:cache
 
-  # Optimize configuration loading
-  php artisan config:cache
-
-  # Optimize route loading
-  php artisan route:cache
-EOF
+# Optimize route loading
+php artisan route:cache
