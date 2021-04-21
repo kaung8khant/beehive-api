3f6e111479e5696e73adec5ba4f8d77103436e16
Wut Yee Tun <wutyee.tun@innovate.com>
2021-04-21 10:33:38 +0630

    create address in address controller

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,6 +7,8 @@ use App\Helpers\ResponseHelper;
 use App\Helpers\StringHelper;
 use App\Models\Address;
 use App\Models\Customer;
+use App\Models\Township;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
@@ -19,5 +21,60 @@ class AddressController extends Controller
 
         $addresses = Address::with('township')->where('customer_id', $customerId)->paginate(10)->items();
         return $this->generateResponse($addresses, 200);
+    }
+
+    public function store(Request $request, $slug)
+    {
+        $request['slug'] = $this->generateUniqueSlug();
+
+        $validator = Validator::make($request->all(), $this->getParamsToValidate(true));
+        if ($validator->fails()) {
+            return $this->generateResponse($validator->errors()->first(), 422, true);
+        }
+
+        $validatedData = $validator->validated();
+
+        $customer= Customer::where('slug', $slug)->firstOrFail();
+        $validatedData['customer_id'] = $customer->id;
+        $validatedData['is_primary'] = true;
+
+        if ($validatedData['township_slug']) {
+            $validatedData['township_id'] = Township::where('slug', $validatedData['township_slug'])->first()->id;
+        }
+
+        $this->setNonPrimary($customer->id);
+
+        $address = Address::create($validatedData);
+        return $this->generateResponse($address->refresh()->load('township'), 201);
+    }
+
+    private function getParamsToValidate($slug = false)
+    {
+        $params = [
+            'label' => 'required',
+            'house_number' => 'required',
+            'floor' => 'nullable|min:0|max:50',
+            'street_name' => 'required',
+            'latitude' => 'nullable',
+            'longitude' => 'nullable',
+            'township_slug' => 'nullable|exists:App\Models\Township,slug',
+        ];
+
+        if ($slug) {
+            $params['slug'] = 'required|unique:addresses';
+        }
+
+        return $params;
+    }
+
+    public function getAllTownships(Request $request)
+    {
+        $townships = Township::paginate($request->size)->items();
+        return $this->generateResponse($townships, 200);
+    }
+
+    private function setNonPrimary($id)
+    {
+        Address::where('customer_id', $id)->update(['is_primary' => 0]);
     }
 }
