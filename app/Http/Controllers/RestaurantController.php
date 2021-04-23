0f1d09008055fbbc73baa87b137cd01900309e8a
Aung Thu Moe <aungthu.moe@hiveinnovate.com>
2021-04-23 15:51:11 +0630

    merged conflicts

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -325,11 +325,6 @@ class RestaurantController extends Controller
             'slugs.*' => 'required|exists:App\Models\Restaurant,slug',
         ]);
 
-<<<<<<< HEAD
-        foreach ($validatedData['restaurants'] as $data) {
-            $restaurant = Restaurant::where('slug', $data['slug'])->firstOrFail();
-            $restaurant->is_enable = $data['is_enable'];
-=======
         foreach ($validatedData['slugs'] as $slug) {
             $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
             if ($request->type === 'enable') {
@@ -337,7 +332,6 @@ class RestaurantController extends Controller
             } else {
                 $restaurant->is_enable = false;
             }
->>>>>>> 3f61378909b479c6a91bb703e43b2388dfa3564f
             $restaurant->save();
         }
 
