321338197a4c58c386c90e9f2425474a176664ff
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-05-24 18:14:13 +0630

    get category image from product and menu

--- app/Models/ShopCategory.php
+++ app/Models/ShopCategory.php
@@ -28,8 +28,10 @@ class ShopCategory extends BaseModel
 
     public function getImagesAttribute()
     {
+        $productId = Product::where('shop_category_id', $this->id)->inRandomOrder()->first();
+
         return File::where('source', 'shop_categories')
-            ->where('source_id', $this->id)
+            ->where('source_id', $productId)
             ->where('type', 'image')
             ->whereIn('extension', ['png', 'jpg'])
             ->get();
