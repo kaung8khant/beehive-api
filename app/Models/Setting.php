184a31a6434f053682fa01d682901048aa5b77fa
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-05-24 17:15:40 +0630

    remove Setting swagger

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,16 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-/**
- * @OA\Schema(
- *      @OA\Xml(name="Setting"),
- *       @OA\Items(type="object",
- *       @OA\Property(property="key", type="string", example="tax"),
- *       @OA\Property(property="value", type="string", example="20"),
- *       @OA\Property(property="data_type", type="string", example="integer")
- *   ),
- * )
- */
 class Setting extends BaseModel
 {
     use HasFactory;
