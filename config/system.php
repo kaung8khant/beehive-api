2557a35c560aec01d7cc9fb89f0bea1d7bf94d76
Aung Thu Moe <aungthu.moe@hiveinnovate.com>
2021-07-11 22:32:02 +0630

    working on messaging

--- config/system.php
+++ config/system.php
@@ -3,5 +3,6 @@
 return [
     'restaurant_search_radius' => env('RESTAURANT_SEARCH_RADIUS', 3),
     'boomsms_api_key' => env('BOOMSMS_API_KEY'),
+    'slack_webhook_url' => env('SLACK_WEB_HOOK_URL'),
     'build_number' => env('BUILD_NUMBER', 1),
 ];
