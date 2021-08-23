fa9146192d421269a219f3a35473d3b7126b97fb
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-08-23 13:14:25 +0630

    restaurant add to cart

--- app/Http/Controllers/Cart/RestaurantCartController.php
+++ app/Http/Controllers/Cart/RestaurantCartController.php
@@ -8,9 +8,12 @@ use App\Helpers\StringHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\Menu;
+use App\Models\MenuCart;
+use App\Models\MenuCartItem;
 use App\Models\MenuTopping;
 use App\Models\MenuVariant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class RestaurantCartController extends Controller
@@ -36,35 +39,56 @@ class RestaurantCartController extends Controller
         $customer = Customer::where('slug', $request->customer_slug)->firstOrFail();
 
         try {
-            $menuVariant = $this->getVariant($menu, $request->variant);
-            $toppings = $this->getToppings($menu, $request->toppings);
+            $menuCart = MenuCart::where('customer_id', $customer->id)->first();
+            $menuData = $this->prepareMenuData($menu, $request);
 
-            $data = [
-                'slug' => StringHelper::generateUniqueSlug(),
-                'amount' => $menuVariant->price,
-                'tax' => $menuVariant->tax,
-                'discount' => $menuVariant->discount,
-                'promocode' => null,
-                'promocode_amount' => 0,
-                'commission' => 0,
-                'total_amount' => $menuVariant->price,
-                'menus' => [
-                    [
-                        'slug' => $menu->slug,
-                        'name' => $menu->name,
-                        'description' => $menu->description,
-                        'variant' => $menuVariant,
-                        'toppings' => $toppings,
-                    ],
-                ],
-                'address' => $customer->primary_address,
-            ];
+            if ($menuCart) {
+                $menuCartItem = MenuCartItem::where('menu_cart_id', $menuCart->id)->where('menu_id', $menu->id)->first();
+
+                if ($menuCartItem) {
+                    $menuCartItem->menu = $menuData;
+                    $menuCartItem->save();
+                } else {
+                    $this->createMenuCartItem($menuCart->id, $menu->id, $menuData);
+                }
+            } else {
+                $menuCart = DB::transaction(function () use ($customer, $menu, $menuData) {
+                    $menuCart = $this->createMenuCart($customer->id);
+                    $this->createMenuCartItem($menuCart->id, $menu->id, $menuData);
+
+                    return $menuCart;
+                });
+            }
+
+            $data = $this->prepareMenuCartData($menuCart->load('menuCartItems'), $customer);
 
             return $this->generateResponse($data, 200);
 
         } catch (BadRequestException $e) {
             return $this->generateResponse($e->getMessage(), 400, true);
         }
+    }
+
+    private function prepareMenuData($menu, $request)
+    {
+        $menuVariant = $this->getVariant($menu, $request->variant);
+        $toppings = $this->getToppings($menu, $request->toppings);
+
+        $amount = $menuVariant->price + collect($toppings)->sum('price');
+        $tax = ($amount - $menuVariant->discount) * $menuVariant->tax * 0.01;
+        $discount = $menuVariant->discount;
+
+        return [
+            'slug' => $menu->slug,
+            'name' => $menu->name,
+            'description' => $menu->description,
+            'amount' => $amount,
+            'tax' => $tax,
+            'discount' => $discount,
+            'quantity' => $request->quantity,
+            'variant' => $menuVariant,
+            'toppings' => $toppings,
+        ];
     }
 
     private function getVariant($menu, $variant)
@@ -98,6 +122,60 @@ class RestaurantCartController extends Controller
 
         return $menuToppings;
     }
+
+    private function createMenuCart($customerId)
+    {
+        return MenuCart::create([
+            'slug' => StringHelper::generateUniqueSlug(),
+            'customer_id' => $customerId,
+        ]);
+    }
+
+    private function createMenuCartItem($menuCartId, $menuId, $menuData)
+    {
+        MenuCartItem::create([
+            'menu_cart_id' => $menuCartId,
+            'menu_id' => $menuId,
+            'menu' => $menuData,
+        ]);
+    }
+
+    private function prepareMenuCartData($menuCart, $customer)
+    {
+        return [
+            'slug' => $menuCart->slug,
+            'promocode' => $menuCart->promocode,
+            'promo_amount' => $menuCart->promo_amount,
+            'total_amount' => $this->getTotalAmount($menuCart->menuCartItems->pluck('menu'), $menuCart->promo_amount),
+            'menus' => $menuCart->menuCartItems->pluck('menu'),
+            'address' => $customer->primary_address,
+        ];
+    }
+
+    private function getTotalAmount($menuCartItems, $promoAmount)
+    {
+        $totalAmount = 0;
+
+        foreach ($menuCartItems as $item) {
+            $amount = ($item['amount'] + $item['tax'] - $item['discount']) * $item['quantity'];
+            $totalAmount += $amount;
+        }
+
+        return $totalAmount - $promoAmount;
+    }
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
 
     public function update(Request $request, Menu $menu)
     {
