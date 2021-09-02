408193beaecd80d0715a0cc2b92437f2738c7268
tuntinzarthin <tuntinzar.thin@hiveinnovate.com>
2021-09-02 15:29:37 +0630

    restaurant invoice pdf

--- app/Http/Controllers/Pdf/RestaurantInvoiceController.php
+++ app/Http/Controllers/Pdf/RestaurantInvoiceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Pdf;
 use App\Http\Controllers\Controller;
 use App\Models\RestaurantOrder;
 use Barryvdh\DomPDF\Facade as PDF;
+use Carbon\Carbon;
 
 class RestaurantInvoiceController extends Controller
 {
@@ -12,12 +13,15 @@ class RestaurantInvoiceController extends Controller
     {
         $restaurantOrder = RestaurantOrder::where('slug', $slug)->firstOrFail();
         $branchInfo = $restaurantOrder->restaurant_branch_info;
+        $restaurantOrderItems = $restaurantOrder->restaurantOrderItems;
+        $restaurantOrderContact = $restaurantOrder->restaurantOrderContact;
+        $date = Carbon::parse($restaurantOrder->order_date)->format('d M Y');
 
         $fileName = $restaurantOrder->slug . '-' . $restaurantOrder->invoice_id . '.pdf';
 
         // return $restaurantOrder->restaurant_branch_info;
 
-        $pdf = PDF::loadView('restaurant-invoice', compact('restaurantOrder', 'branchInfo'))->setPaper('a4');
+        $pdf = PDF::loadView('restaurant-invoice', compact('restaurantOrder', 'branchInfo', 'restaurantOrderItems', 'restaurantOrderContact', 'date'))->setPaper('a4');
         return $pdf->download($fileName);
     }
 }
