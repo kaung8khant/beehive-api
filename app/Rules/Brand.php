d81b4bda5705dff2223873364c550cdfb0498bc6
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-09-10 11:12:11 +0630

    customer group promo code rule added

--- app/Rules/Brand.php
+++ app/Rules/Brand.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 
 class Brand implements Rule
 {
+    private $promocode;
+    private $usage;
 
     public function __construct($promocode, $usage)
     {
@@ -15,28 +17,23 @@ class Brand implements Rule
 
     public function validate($items, $subTotal, $customer, $value): bool
     {
-
         if ($this->usage == "shop") {
             foreach ($items as $item) {
-
                 $product = Product::where('slug', $item['slug'])->with('brand')->firstOrFail();
-
                 $brand_id = $product->brand->id;
+
                 if (intval($value) == intval($brand_id)) {
                     return true;
                 }
-
             }
         }
         return false;
-
     }
+
     public function validateItem($item, $value): bool
     {
         if ($this->usage == "shop") {
-
             $product = Product::where('slug', $item['slug'])->with('brand')->firstOrFail();
-
             $brand_id = $product->brand->id;
             return intval($value) == intval($brand_id);
         }
