7a0384028f3d7f6a688ea2ae4c3cb91f7d05d11d
Htun Aung Lin <htunaung.lin@hiveinnovate.com>
2021-11-15 20:47:06 +0630

    add credit route for customer

---
+++ app/Http/Controllers/Customer/v3/CreditController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Customer\v3;
+
+use App\Helpers\ResponseHelper;
+use App\Helpers\v3\OrderHelper;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use stdClass;
+
+class CreditController extends Controller
+{
+    use ResponseHelper;
+
+    public function index()
+    {
+        $customer = Auth::guard('customers')->user();
+        $credit = $customer->credit;
+
+        if ($credit) {
+            $credit->remaining_amount = OrderHelper::getRemainingCredit($customer);
+            return $this->generateResponse($credit, 200);
+        }
+
+        return $this->generateResponse(new stdClass, 200);
+    }
+}
