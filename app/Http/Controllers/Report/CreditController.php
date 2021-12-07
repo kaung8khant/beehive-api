8238880d351b34d261106204a59bc86445407a05
tuntinzarthin <tuntinzar.thin@hiveinnovate.com>
2021-12-07 16:12:12 +0630

    credit customer sales with invoice no,date and order no

--- app/Http/Controllers/Report/CreditController.php
+++ app/Http/Controllers/Report/CreditController.php
@@ -40,7 +40,9 @@ class CreditController extends Controller
 
             $data[] = [
                 'order_no' => $order->order_no,
+                'invoice_no' => $order->invoice_no,
                 'order_date' => Carbon::parse($order->order_date)->format('M d Y h:i a'),
+                'invoice_date' =>$order->invoice_date? Carbon::parse($order->invoice_date)->format('M d Y h:i a') : null,
                 'revenue' => $amount,
                 'commercial_tax' => $order->order_status != 'cancelled' && $order->tax ? $order->tax : 0,
                 'discount' => $order->order_status != 'cancelled' && $order->discount ? $order->discount : 0,
