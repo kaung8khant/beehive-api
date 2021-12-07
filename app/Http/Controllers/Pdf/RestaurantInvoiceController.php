534a145f40ea1d660930af70b0db3ece0791b4cc
tuntinzarthin <tuntinzar.thin@hiveinnovate.com>
2021-12-07 11:16:19 +0630

    order report

--- app/Http/Controllers/Pdf/RestaurantInvoiceController.php
+++ app/Http/Controllers/Pdf/RestaurantInvoiceController.php
@@ -5,31 +5,19 @@ namespace App\Http\Controllers\Pdf;
 use App\Events\RestaurantOrderUpdated;
 use App\Http\Controllers\Controller;
 use App\Models\RestaurantOrder;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 
 class RestaurantInvoiceController extends Controller
 {
     const PATH = 'pdf/restaurants/';
 
-    // public function generateInvoice($slug)
-    // {
-    //     $restaurantOrder = RestaurantOrder::where('slug', $slug)->firstOrFail();
-    //     event(new RestaurantOrderUpdated($restaurantOrder));
-
-    //     $fileName = $restaurantOrder->slug . '-' . $restaurantOrder->invoice_id . '.pdf';
-    //     return Storage::download(self::PATH . $fileName);
-    // }
-
-
     public function generateInvoice($slug)
     {
         $restaurantOrder = RestaurantOrder::where('slug', $slug)->firstOrFail();
-        $branchInfo = $restaurantOrder->restaurant_branch_info;
-        $restaurantOrderItems = $restaurantOrder->restaurantOrderItems;
-        $restaurantOrderContact = $restaurantOrder->restaurantOrderContact;
-        $date = Carbon::parse($restaurantOrder->order_date)->format('d M Y');
-        return view('restaurant-invoice', compact('restaurantOrder', 'branchInfo', 'restaurantOrderItems', 'restaurantOrderContact', 'date'));
+        event(new RestaurantOrderUpdated($restaurantOrder));
+
+        $fileName = $restaurantOrder->slug . '-' . $restaurantOrder->invoice_id . '.pdf';
+        return Storage::download(self::PATH . $fileName);
     }
 
     public function getInvoice($fileName)
